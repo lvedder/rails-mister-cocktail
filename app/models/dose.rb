acf42a505391a4fa12c5dc5509e9bd11ee13738c
Lamina Vedder <lvedder@mail.uni-mannheim.de>
2018-11-23 11:11:28 +0100

    completed controller setup

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,5 @@
 class Dose < ApplicationRecord
-  validates :description, presence: true, allow_blank: false
+  validates :description, presence: true
   validates :ingredient, uniqueness: { scope: [:cocktail] }
   belongs_to :cocktail
   belongs_to :ingredient
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "grenadine")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "egg white")
+Ingredient.create(name: "whiskey")
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "caipirinha")
+Cocktail.create(name: "mai tai")
+Cocktail.create(name: "sex on the beach")
+Cocktail.create(name: "munich mule")
+Cocktail.create(name: "moscow mule")
+Cocktail.create(name: "basil smash")
+Cocktail.create(name: "whiskey sour")
+Cocktail.create(name: "mint julep")
+
+
+
