624ae32f167d6f92e17567ecccc9a57115d91c5f
Lamina Vedder <lvedder@mail.uni-mannheim.de>
2018-11-23 16:09:03 +0100

    corrected root

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get "cocktails/:cocktail_id/doses/new", to: "doses#new", as: "new_doses"
   post "cocktails/:cocktail_id/doses", to: "doses#create", as: :doses
   delete "doses/:id", to: "doses#destroy"
-  root to: "controller#action"
+  root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
