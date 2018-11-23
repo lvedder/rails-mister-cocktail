4168ad46adc3501f3a29f0f199ffd2689fbbd39d
Lamina Vedder <lvedder@mail.uni-mannheim.de>
2018-11-23 16:23:30 +0100

    made new doses work

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,9 +6,10 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_doses_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
